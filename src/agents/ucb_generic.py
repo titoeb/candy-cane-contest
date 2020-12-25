684dd84409d778248978fa6c35253805403a0c2b
titoeb <timtoebrock@gmail.com>
2020-12-25 12:24:44 +0100

    Created UCB generic

---
+++ src/agents/ucb_generic.py
@@ -0,0 +1,158 @@
+#! pip install kaggle-environments --upgrade -q
+
+# PARAMS #
+EXPLORATION = 0.45
+SAMPLING = True
+DECAYING = 0.97
+BUFFER_LENTH = 100
+DAMPING_FACTOR = 4
+MIN_STEP_OPPONENT = 500
+# PARAMS #
+
+import numpy as np
+import random
+import collections
+
+
+class UpperConfidenceBound:
+    def __init__(
+        self,
+        c,
+        n_bins,
+        sampling,
+        decaying,
+        buffer_length,
+        damping_factor,
+        min_step_opponent,
+    ):
+        self._c = c
+        self._n_bins = n_bins
+        self._sampling = sampling
+        self._decaying = decaying
+        self._buffer_length = buffer_length
+        self._damping_factor = damping_factor
+        self._min_step_opponent = min_step_opponent
+        self.initiallize(
+            n_bins=n_bins,
+            c=c,
+            sampling=sampling,
+            decaying=decaying,
+            buffer_length=buffer_length,
+            damping_factor=damping_factor,
+            min_step_opponent=min_step_opponent,
+        )
+
+    def initiallize(
+        self,
+        n_bins,
+        c,
+        sampling,
+        decaying,
+        buffer_length,
+        damping_factor,
+        min_step_opponent,
+    ):
+        self.c = c
+        self.decaying = decaying
+        self.sampling = sampling
+        self.buffer_length = buffer_length
+        self.damping_factor = damping_factor
+        self.n_bins = n_bins
+        self.min_step_opponent = min_step_opponent
+
+        self.opponent_action_buffer = collections.deque(maxlen=buffer_length)
+        self.n_used = np.full(n_bins, 1e-10)
+        self.rewards = np.full(n_bins, 1e-10)
+        self.total_reward = 0
+
+    def play(self, observation, configuration):
+        my_index = observation.agentIndex
+        if observation.step == 0:
+            self.initiallize(
+                n_bins=self._n_bins,
+                c=self._c,
+                sampling=self._sampling,
+                decaying=self._decaying,
+                buffer_length=self._buffer_length,
+                damping_factor=self._damping_factor,
+                min_step_opponent=self._min_step_opponent,
+            )
+        else:
+            # Extract info from observation.
+            my_last_action = observation.lastActions[my_index]
+            opponent_last_action = observation.lastActions[1 - my_index]
+            reward = observation.reward - self.total_reward
+
+            # Extract params
+            self.opponent_action_buffer.append(opponent_last_action)
+            self.rewards[my_last_action] += reward
+            self.n_used[my_last_action] += 1
+            self.total_reward = observation.reward
+
+            if self.decaying:
+                self.rewards[my_last_action] *= self.decaying
+                self.rewards[opponent_last_action] *= self.decaying
+
+        # Choose action
+        # Compute ucb target function
+        success_ratio = self.rewards / self.n_used
+        t = observation.step
+        exploration = self.c * np.sqrt(np.log(t + 1) / self.n_used)
+        target_function = success_ratio + exploration
+
+        # If the opponent used the action recently (e.g. it is in the buffer)
+        # incraese the target function multiplicativly.
+        if self.damping_factor and t > self.min_step_opponent:
+            recent_actions = set(self.opponent_action_buffer)
+            values = (
+                1
+                + np.log(
+                    [
+                        1 + self.opponent_action_buffer.count(recent_action)
+                        for recent_action in recent_actions
+                    ]
+                )
+                / self.damping_factor
+            )
+
+            factors = np.ones(self.n_bins)
+            factors[list(recent_actions)] *= values
+
+            target_function = target_function * factors
+
+        optimal_action = np.argmax(target_function)
+
+        action_is_optimal_mask = target_function == target_function[optimal_action]
+        if action_is_optimal_mask.sum() > 1 and self.sampling:
+            # Sample a random one from the optimal actions
+            return int(np.random.choice(np.where(action_is_optimal_mask)[0]))
+
+        return int(optimal_action)
+
+
+upper_confidence_bound = UpperConfidenceBound(
+    n_bins=100,
+    c=EXPLORATION,
+    sampling=SAMPLING,
+    decaying=DECAYING,
+    buffer_length=BUFFER_LENTH,
+    damping_factor=DAMPING_FACTOR,
+    min_step_opponent=MIN_STEP_OPPONENT,
+)
+
+
+def agent(observation, configuration):
+    return upper_confidence_bound.play(observation, configuration)
+
+
+# if __name__ == "__main__":
+#     from kaggle_environments import make
+
+#     def random_agent(observation, configuration):
+#         return random.randrange(configuration.banditCount)
+
+#     env = make("mab", debug=True)
+
+#     # Run environment
+#     steps = env.run([random_agent, agent])
+#     print(steps[-1][0].reward, steps[-1][1].reward)
--- src/debug.py
+++ src/debug.py
@@ -1,7 +1,6 @@
 from utils import play_mab, simulate_mab
-from agent_random import agent as agent_random
-from agent_vegas_slot_machines import agent as agent_slot_machine
-from agent_kaggle_epsilon_greedy_decay import multi_armed_probabilities
+from agents.random import agent as random_agent
+from agents_broken.ucb import agent as agent_usb
 
 if __name__ == "__main__":
     # greedy_agent = GreedyAgent(n_bins=100)
@@ -9,7 +8,7 @@ if __name__ == "__main__":
     # n_bins=100, epsilon_start=1.0, ratio_decay=0.9997, min_epsilon=0.4
     # )
 
-    AGENTS = [multi_armed_probabilities, agent_slot_machine]
+    AGENTS = [random_agent, agent_usb]
 
     # print(play_mab(agents=[cycle_agent, greedy_agent.play]))
     res = simulate_mab(agents=AGENTS, n_rounds=1, n_processes=1)

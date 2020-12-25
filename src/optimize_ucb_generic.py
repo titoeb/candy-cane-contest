07261b2c6b542738786bdade3b53ef1db4dce560
titoeb <timtoebrock@gmail.com>
2020-12-25 17:04:55 +0100

    Updated gitignore

--- src/optimize_ucb_generic.py
+++ src/optimize_ucb_generic.py
@@ -2,6 +2,7 @@ from evaluate import main as evaluate_main
 import numpy as np
 import re
 import optuna
+import datetime
 
 # Params
 OPPONENT_POOL = [
@@ -23,7 +24,7 @@ def optimize_ucb_generic(trial):
     # Run candidate against pool
 
     # Report 1 - win-ratio as target variable
+    return 1.0
 
-
-study = optuna.create_study()
-study.optimize(objective, n_trials=N_EVALS)
+study = optuna.create_study(storage=f"sqlite:///optuna/run_{strftime("%d-%m-%Y--%H-%M-%S")}.db")
+study.optimize(optimize_ucb_generic n_trials=N_EVALS)

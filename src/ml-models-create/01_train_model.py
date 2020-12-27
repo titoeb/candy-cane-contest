062ac93cb37b475667843253b9877255489c647a
titoeb <timtoebrock@gmail.com>
2020-12-27 17:03:12 +0100

    Started model building

--- src/ml-models-create/01_train_model.py
+++ src/ml-models-create/01_train_model.py
@@ -1,11 +1,7 @@
-import pandas as pd 
+import pandas as pd
 import sklearn as sk
 
-
-DATA_FILE = 
-
-
-
+DATA_FILE = "/usr/src/data/training_data_kaggle.parquet"
 
 data = pd.read_parquet(DATA_FILE)
 model = sk.DecisionTreeRegressor(min_samples_leaf=40)

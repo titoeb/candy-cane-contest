4bd0232d15e42cf226fc3c0d0274ead253c87cd4
titoeb <timtoebrock@gmail.com>
2021-01-01 09:58:41 +0100

    Added file name post fix for created agents

--- src/ml-models-create/02_create_agent_file.py
+++ src/ml-models-create/02_create_agent_file.py
@@ -1,6 +1,6 @@
 ML_MODEL_BASE_FILE = "/usr/src/ml-models-base/greedy_base.py"
 MODEL_FILE = "/usr/src/models/decision_tree.txt"
-AGENT_FILE_PATH = "/usr/src/agents/ml_agent_decision_tree.py"
+AGENT_FILE_PATH = "/usr/src/agents/ml_agent_decision_tree"
 
 with open(ML_MODEL_BASE_FILE) as file_handler:
     ml_model_base = file_handler.read()
@@ -11,6 +11,7 @@ with open(MODEL_FILE) as file_handler:
 
 
 final_agent = ml_model + "\n" + ml_model_base
+file_name = f'{AGENT_FILE_PATH}{datetime.datetime.now().strftime("_%Y_%m_%d__%H_%M_%S")}.py'
 
-with open(AGENT_FILE_PATH, "w") as file_handler:
+with open(file_name, "w") as file_handler:
     file_handler.write(final_agent)
